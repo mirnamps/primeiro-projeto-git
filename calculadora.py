4c68071c792fb1ed54837fe1b84253dad819b681
Mirna Pereira Silva <mirnamps@gmail.com>
2026-04-21 16:35:10 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+# Minha primeira calculadora
+print("--- Calculadora de Soma ---")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print(f"O resultado da soma é: {soma}")
